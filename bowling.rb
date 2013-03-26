1e478003f6ecd5f5a4cb9de931333beeeaf5b87a
poppen <poppen.jp@gmail.com>
2013-03-26 18:28:04 +0900

    Implement spare and next hits 3 pins

--- bowling.rb
+++ bowling.rb
@@ -1,13 +1,24 @@
 class Bowling
   def initialize
     @score = 0
+    @throws = []
   end
 
   def hit(pins)
-    @score += pins
+    @throws.push pins
   end
 
   def score
+    previous_spare = false
+    @throws.each_slice(2) do |frame|
+      if previous_spare
+        @score += frame[0]
+      end
+      previous_spare = (frame[0] + frame[1] == 10 ? true : false)
+
+      @score += (frame[0] + frame[1])
+    end
+
     @score
   end
 end
